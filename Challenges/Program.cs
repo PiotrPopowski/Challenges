07035db26ef7f455853ab2704981247dd4aa762f
popo <p.popowski01@gmail.com>
2024-01-23 21:42:41 +0100

    csv reader

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Challenges
+﻿using Challenges.Models;
+using Challenges.Services;
+
+namespace Challenges
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var filePath = Path.GetFullPath("Utility/product.csv");
+            var parser = new CSVParser();
+            var products = parser.ReadCsv<Product>(filePath, new CSVParsingOptions()).ToList();
+
+            products.Add(new Product()
+            {
+                Id = 3,
+                Name = "Banana",
+                Price = 2.69m
+            });
+
+            parser.SaveCsv(products, filePath, new CSVParsingOptions());
+
+            foreach(var product in products)
+                Console.WriteLine(product);
         }
     }
 }
---
+++ Challenges/Services/CSVParser.cs
@@ -0,0 +1,52 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace Challenges.Services
+{
+    public class CSVParser
+    {
+        public IEnumerable<T> ReadCsv<T>(string filePath, CSVParsingOptions options)
+            where T : class, new()
+        {
+            var config = new CsvConfiguration(options.Culture);
+            config.HasHeaderRecord = options.HasHeaders;
+            config.Delimiter = options.Delimiter;
+            config.ReadingExceptionOccurred = x => options.OnError.Invoke(x.Exception);
+
+            using var reader = new StreamReader(filePath);
+            using var csv = new CsvReader(reader, config);
+            var records = csv.GetRecords<T>();
+
+            return records.ToList();
+        }
+
+        public void SaveCsv<T>(IEnumerable<T> records, string filePath, CSVParsingOptions options)
+            where T : class, new()
+        {
+            var config = new CsvConfiguration(options.Culture);
+            config.Delimiter = options.Delimiter;
+            config.ReadingExceptionOccurred = x => options.OnError.Invoke(x.Exception);
+
+            using var writer = new StreamWriter(filePath);
+            using var csv = new CsvWriter(writer, config);
+            csv.WriteRecords<T>(records);
+        }
+    }
+
+    public class CSVParsingOptions
+    {
+        public CultureInfo Culture { get; set; }
+        public string Delimiter { get; set; }
+        public Func<Exception, bool> OnError { get; set; }
+        public bool HasHeaders { get; set; }
+
+        public CSVParsingOptions()
+        {
+            Culture = CultureInfo.InvariantCulture;
+            Delimiter = ";";
+            OnError = x => throw x;
+            HasHeaders = false;
+        }
+    }
+}
