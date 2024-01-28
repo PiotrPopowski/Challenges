c1ae221bbda5c4add1fe1e8375c7edf01b3444b0
popo <p.popowski01@gmail.com>
2024-01-28 21:16:54 +0100

    caesar

---
+++ Ceaser.UnitTesting/CeaserTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Ceaser.UnitTesting
+{
+    public class CeaserTests
+    {
+        [Theory]
+        [InlineData("A", "E", 4)]
+        [InlineData("year", "AGCT", 2)]
+        [InlineData("Today is a great day.", "NIXUS CM U ALYUN XUS.", 20)]
+        public void ceaser_alghoritm_encodes_given_text(string text, string encodedText, uint shift)
+        {
+            var ceaser = new Challenges.Alghoritms.Ceaser(shift);
+
+            var encoded = ceaser.Encode(text);
+            
+            Assert.Equal(encodedText, encoded);
+        }
+
+        [Theory]
+        [InlineData("E", "A", 4)]
+        [InlineData("AGCT", "YEAR", 2)]
+        [InlineData("NIXUS CM U ALYUN XUS.", "TODAY IS A GREAT DAY.", 20)]
+        public void ceaser_alghoritms_decodes_given_text(string encodedText, string decodedText, uint shift)
+        {
+            var ceaser = new Challenges.Alghoritms.Ceaser(shift);
+
+            var text = ceaser.Decode(encodedText);
+
+            Assert.Equal(decodedText, text);
+        }
+    }
+}
---
+++ Challenges/Alghoritms/Ceaser.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace Challenges.Alghoritms
+{
+    public class Ceaser
+    {
+        private readonly uint _shift;
+        private static char[] Alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        private static char[] PunctuationMarks = [' ', '.'];
+
+        public Ceaser(uint shift)
+        {
+            if (shift > 27 || shift == 0) throw new ArgumentException("Ceaser shift must be in range 0-26.");
+            _shift = shift;
+        }
+
+        public string Encode(string text)
+        {
+            text = text.ToUpper();
+            if (!CanBeEncoded(text))
+            {
+                throw new Exception("Cannot encode text. Detected unsupported characters.");
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in text)
+            {
+                var encodedC = PunctuationMarks.Contains(c) ? c : Alphabet[encode(c)];
+                sb.Append(encodedC);
+            }
+
+            return sb.ToString();
+
+            uint encode(char c) => (uint)((Array.IndexOf(Alphabet, c) + _shift) % Alphabet.Length);
+        }
+
+        public string Decode(string text)
+        {
+            text = text.ToUpper();
+            if (!CanBeEncoded(text))
+            {
+                throw new Exception("Cannot decode text. Detected unsupported characters.");
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in text)
+            {
+                var decodedC = PunctuationMarks.Contains(c) ? c : Alphabet[decode(c)];
+                sb.Append(decodedC);
+            }
+
+            return sb.ToString();
+
+            uint decode(char c) => (uint)((Array.IndexOf(Alphabet, c) - _shift + Alphabet.Length) % Alphabet.Length);
+        }
+
+        private static bool CanBeEncoded(string text) => text.Except(PunctuationMarks).All(Alphabet.Contains);
+    }
+}
