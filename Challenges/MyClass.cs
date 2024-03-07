41dff7362aa1c8df76665cc27da22b5216309169
popo <p.popowski01@gmail.com>
2024-03-07 20:11:04 +0100

    serialziation

---
+++ Challenges/MyClass.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Challenges
+{
+    internal class MyClass<T> where T : class
+    {
+        public Dictionary<string, string> dic { get; set; } = new Dictionary<string, string>();
+        public T Value { get; }
+
+        public MyClass(T value)
+        {
+            Value = value;
+        }
+    }
+}
--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Challenges
+﻿using System.Text.Json;
+
+namespace Challenges
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MyClass<string> mc = new MyClass<string>("Hi");
+
+            mc.dic["key"] = "value";
+
+            var json =  JsonSerializer.Serialize(mc);
+
+            var newDic = JsonSerializer.Deserialize<MyClass<string>>(json);
         }
     }
 }
